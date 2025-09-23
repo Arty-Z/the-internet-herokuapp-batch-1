d23a69e9e48b5f1326ce9c5ed668a28d10c1551e
Arty-Z <arty.zhuria@gmail.com>
2025-09-22 20:49:29 -0500

    formated

--- pages/the-internet-home-page.ts
+++ pages/the-internet-home-page.ts
@@ -3,10 +3,10 @@ import { expect, Locator, Page } from '@playwright/test'
 export class HomePage {
     textHeader: Locator
 
-constructor(page:Page){
-    this.textHeader = page.getByRole('heading', { name: 'Welcome to the-internet' })
+    constructor(page: Page) {
+        this.textHeader = page.getByRole('heading', { name: 'Welcome to the-internet' })
     }
     async validateHeader(): Promise<void> {
         await expect(this.textHeader).toBeVisible();
     }
-}
+} 
--- tests/the-internet-test.spec.ts
+++ tests/the-internet-test.spec.ts
@@ -6,6 +6,6 @@ test('Header verification', async ({ page }) => {
     let homePage = new HomePage(page);
 
     await page.goto('https://the-internet.herokuapp.com/');
-   
+
     await homePage.validateHeader();
 })
