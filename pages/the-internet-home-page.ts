89bf8d2b3214a0c855be698c7d02cd4402651e19
Arty-Z <arty.zhuria@gmail.com>
2025-09-23 19:12:08 -0500

    first commit

--- pages/the-internet-home-page.ts
+++ pages/the-internet-home-page.ts
@@ -2,11 +2,22 @@ import { expect, Locator, Page } from '@playwright/test'
 
 export class HomePage {
     textHeader: Locator
+    dropDownLink: Locator
+    dropDown: Locator
 
     constructor(page: Page) {
         this.textHeader = page.getByRole('heading', { name: 'Welcome to the-internet' })
+        this.dropDownLink = page.getByRole('link', { name: 'Dropdown' })
+        this.dropDown = page.locator('#dropdown')
     }
     async validateHeader(): Promise<void> {
         await expect(this.textHeader).toBeVisible();
     }
-} 
+    async clickDropDownLink(): Promise<void> {
+        await this.dropDownLink.click();
+    }
+    async selectDropDownOption(option: string): Promise<void> {
+        await this.dropDown.selectOption({ label: option });
+    } 
+}
+
---
+++ tests/homePage-dropDown.spec.ts
@@ -0,0 +1,13 @@
+import { test } from '@playwright/test'
+import { HomePage } from '../pages/the-internet-home-page'
+
+test('DropDown verification', async ({ page }) => {
+
+    let homePage = new HomePage(page);
+
+    await page.goto('https://the-internet.herokuapp.com/');
+
+    await homePage.clickDropDownLink();
+    await homePage.selectDropDownOption("Option 1");
+
+})
