fc9d0dbc67a08cdb12b36344fe13dd5f5a1647fd
Arty-Z <arty.zhuria@gmail.com>
2025-09-24 20:39:02 -0500

    last updated

--- pages/forgot-password-page.ts
+++ pages/forgot-password-page.ts
@@ -3,13 +3,13 @@ import { expect, Locator, Page } from '@playwright/test'
 export class ForgotPasswordPage {
     enterEmailField: Locator
     retrievePasswordButton: Locator
-    errorMessage: Locator
+    
 
 
     constructor(page: Page) {
         this.enterEmailField = page.getByRole('textbox', { name: 'E-mail' })
         this.retrievePasswordButton = page.getByRole('button', { name: 'Retrieve password' })
-        this.errorMessage = page.locator('#content > div > h1')
+        
     }
     async enterEmail(email: string): Promise<void> {
         await this.enterEmailField.fill(email);
